4ccefd9759b7aff638a7c4a49b1de983d7d5ad12
Daniel Morandini <danielmorandini@me.com>
2022-04-27 15:22:32 +0200

    Add more metadata

--- lib/flow/telemetry.ex
+++ lib/flow/telemetry.ex
@@ -9,31 +9,30 @@ defmodule Flow.Telemetry do
   # ability to substitute them.
   def instrument(flow = %Flow{}, event_prefix) when is_list(event_prefix) do
     flow
-    |> walk_and_instrument(event_prefix)
+    |> walk_and_instrument(event_prefix, 0)
   end
 
-  defp walk_and_instrument(flow = %Flow{}, event_prefix) do
+  defp walk_and_instrument(flow = %Flow{}, event_prefix, depth) do
     flow
     |> Map.update(:operations, [], fn ops ->
-      Enum.map(ops, &instrument_op(&1, event_prefix))
+      Enum.map(ops, &instrument_op(&1, event_prefix, depth))
     end)
     |> Map.update(:producers, [], fn
       {:flows, flows} ->
-        {:flows, Enum.map(flows, &walk_and_instrument(&1, event_prefix))}
+        {:flows, Enum.map(flows, &walk_and_instrument(&1, event_prefix, depth + 1))}
 
       other ->
         other
     end)
   end
 
-  defp instrument_op({:mapper, id, funs}, event_prefix) do
+  defp instrument_op({:mapper, id, funs}, event_prefix, depth) do
     funs =
       funs
       |> Enum.map(fn fun ->
         fn x ->
           instrument_fun(fun, [x], event_prefix, %{
-            op: :mapper,
-            id: id
+            id: [id, depth]
           })
         end
       end)
@@ -41,10 +40,10 @@ defmodule Flow.Telemetry do
     {:mapper, id, funs}
   end
 
-  defp instrument_op({:reduce, acc_fun, reducer_fun}, event_prefix) do
+  defp instrument_op({:reduce, acc_fun, reducer_fun}, event_prefix, depth) do
     reducer_fun = fn x, acc ->
       instrument_fun(reducer_fun, [x, acc], event_prefix, %{
-        op: :reduce
+        id: [:reduce, depth]
       })
     end
 
@@ -55,7 +54,7 @@ defmodule Flow.Telemetry do
   # * {:uniq, fun()}
   # * {:emit_and_reduce, fun(), fun()}
   # * {:on_trigger, fun()}
-  defp instrument_op(unsupported_op, _event_prefix) do
+  defp instrument_op(unsupported_op, _event_prefix, _depth) do
     unsupported_op
   end
 
@@ -66,13 +65,13 @@ defmodule Flow.Telemetry do
       fn ->
         result = apply(fun, args)
 
-        metadata =
+        count =
           cond do
-            is_list(result) -> %{result_size: Enum.count(result)}
-            true -> %{}
+            is_list(result) -> Enum.count(result)
+            true -> 1
           end
 
-        {result, metadata}
+        {result, %{result_count: count}}
       end
     )
   end
