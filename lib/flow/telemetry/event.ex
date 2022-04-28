888d1e7bc61528ce7df36e012b471948ae37136a
Daniel Morandini <danielmorandini@me.com>
2022-04-28 11:11:53 +0200

    Merge Start and Stop events into a Span

--- lib/flow/telemetry/event.ex
+++ lib/flow/telemetry/event.ex
@@ -1,14 +1,3 @@
-defmodule Flow.Telemetry.SpanEvent do
-  @moduledoc """
-  Generic Telemetry Span event in the Flow context. It brings a time recording
-  together with a reference to its twin event.
-  """
-
-  # It is https://www.erlang.org/doc/man/erlang.html#type-time_unit in reality
-  # but I cannot use it.
-  @type time_unit :: :second | :millisecond | :microsecond | :nanosecond | :native | :perf_counter
-end
-
 defmodule Flow.Telemetry.StartEvent do
   alias Flow.Telemetry.SpanEvent, as: Span
 
@@ -53,6 +42,47 @@ defmodule Flow.Telemetry.StopEvent do
       resolution: :native,
       result_count: count,
       ref: ref
+    }
+  end
+end
+
+defmodule Flow.Telemetry.SpanEvent do
+  alias Flow.Telemetry.StartEvent
+  alias Flow.Telemetry.StopEvent
+
+  # It is https://www.erlang.org/doc/man/erlang.html#type-time_unit in reality
+  # but I cannot use it.
+  @type time_unit :: :second | :millisecond | :microsecond | :nanosecond | :native | :perf_counter
+
+  @type t :: %__MODULE__{
+          id: nonempty_list(),
+          ref: Reference.t(),
+          start_at: integer(),
+          end_at: integer(),
+          duration: integer(),
+          result_count: pos_integer(),
+          resolution: time_unit()
+        }
+  defstruct [:id, :ref, :start_at, :end_at, :duration, :result_count, :resolution]
+
+  def new(%StartEvent{} = start, %StopEvent{} = stop) do
+    if start.ref != stop.ref do
+      raise ArgumentError, "start and stop event references must match"
+    end
+
+    if start.resolution != stop.resolution do
+      raise ArgumentError,
+            "start and stop time resultions must match, have #{start.resolution} on start, #{stop.resolution} on stop"
+    end
+
+    %__MODULE__{
+      id: start.id,
+      ref: start.ref,
+      start_at: start.start_at,
+      end_at: start.start_at + stop.duration,
+      duration: stop.duration,
+      result_count: stop.result_count,
+      resolution: start.resolution
     }
   end
 end
